112f896b884cdab4b461bf1eef32d042785de546
Ruman-Islam <rumanislam48@gmail.com>
2025-02-01 15:04:10 +0600

    filter added

--- src/service/public/system.services.js
+++ src/service/public/system.services.js
@@ -18,7 +18,28 @@ const getTeaTypes = async () => {
 const getSystemConfig = async () => {
   const result = await System.findOne({ systemId: "system-1" });
 
-  return result;
+  const { filters } = result;
+
+  return {
+    ...result.toObject(),
+    filters: [
+      {
+        category: "all",
+        title: "Price",
+        key: "price",
+        options: [
+          { _id: "1", param: "0-25" },
+          { _id: "2", param: "26-40" },
+          { _id: "3", param: "41-65" },
+          { _id: "4", param: "66-75" },
+          { _id: "5", param: "66-85" },
+          { _id: "6", param: "76-95" },
+          { _id: "7", param: "76-100" },
+        ],
+      },
+      ...filters,
+    ],
+  };
 };
 
 export const SystemService = {
